acb43c117ef142152bfe03dcb707f2c6e4ebea30
trnlht <trnlht@gmail.com>
2020-03-24 12:22:32 +0300

    Select button handler added

--- musical_archive_parser_gui.py
+++ musical_archive_parser_gui.py
@@ -1,21 +1,33 @@
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
+from tkinter.filedialog import askdirectory
 
 root = Tk()
 root.title("Musical archives parser")
 
+def on_select_btn_click():
+    directory = askdirectory()
+    if directory != None:
+        ent.insert(0, directory)
+
+
 entry_row = Frame(root)
 
-select_btn = Button(entry_row, text="...")
+select_btn = Button(entry_row, text="...", command=on_select_btn_click)
 select_btn.pack(side=RIGHT)
 
+ent_name = Label(entry_row, text="Directory:")
+ent_name.pack(side=LEFT)
+
 ent = Entry(entry_row)
-ent.pack(side=LEFT, expand=YES, fill=X)
+ent.pack(side=TOP, expand=YES, fill=X)
 
 entry_row.pack(side=TOP, fill=X)
 
+
 st = ScrolledText(root)
 st.pack(side=TOP, expand=YES, fill=BOTH)
+
 
 extract_btn = Button(root, text="Extract")
 extract_btn.pack(side=LEFT)
